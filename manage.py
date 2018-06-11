13e28c080752d4cf949c54c0c1f8a6a968808f91
smartlz <1009098051@qq.com>
2018-06-11 08:55:10 +0800

    添加项目的配置类

--- manage.py
+++ manage.py
@@ -1,8 +1,18 @@
 # coding=utf-8
 from flask import Flask
 
+
+class Config(object):
+    """项目的配置类"""
+    DEBUG = True
+
+
 # 创建Flask应用程序实例
 app = Flask(__name__)
+
+
+# 从配置类中加载配置信息
+app.config.from_object(Config)
 
 
 @app.route('/')
@@ -11,4 +21,4 @@ def index():
 
 if __name__ == '__main__':
     # 运行开发web服务器
-    app.run(debug=True)
+    app.run()
