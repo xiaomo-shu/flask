09c929cf367ba7fb8de8174526d055d54d6918eb
smartlz <1009098051@qq.com>
2018-06-11 09:04:57 +0800

    集成redis数据库

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import redis
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,6 +14,10 @@ class Config(object):
     # 关闭追踪数据库的修改
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # redis数据库的相关配置
+    REDIS_HOST = '172.16.179.139'
+    REDIS_PORT = 6379
+
 
 # 创建Flask应用程序实例
 app = Flask(__name__)
@@ -25,9 +30,14 @@ app.config.from_object(Config)
 # 创建SQLAlchemy的对象
 db = SQLAlchemy(app)
 
+# 创建redis链接对象
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
 
 @app.route('/')
 def index():
+    # 测试redis
+    redis_store.set('name', 'itcast')
     return 'index'
 
 if __name__ == '__main__':
