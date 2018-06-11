4da3e8201cca494fbcb7b1c1f24bf47b1a47c13a
smartlz <1009098051@qq.com>
2018-06-11 08:59:35 +0800

    集成mysql数据库

--- manage.py
+++ manage.py
@@ -1,10 +1,17 @@
 # coding=utf-8
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 class Config(object):
     """项目的配置类"""
     DEBUG = True
+
+    # 数据库相关配置
+    # 设置数据库的链接地址
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@172.16.179.139:3306/gz02_info'
+    # 关闭追踪数据库的修改
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 # 创建Flask应用程序实例
@@ -13,6 +20,10 @@ app = Flask(__name__)
 
 # 从配置类中加载配置信息
 app.config.from_object(Config)
+
+
+# 创建SQLAlchemy的对象
+db = SQLAlchemy(app)
 
 
 @app.route('/')
