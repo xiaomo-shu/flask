46597627b8ce556ae895c345d32716836ee63ad4
smartlz <1009098051@qq.com>
2018-06-17 12:30:35 +0800

    详情页`新闻评论`前端js`回复评论`代码实现

--- info/static/news/js/detail.js
+++ info/static/news/js/detail.js
@@ -214,7 +214,7 @@ $(function(){
 
         if(sHandler.indexOf('reply_sub')>=0)
         {
-            alert('回复评论')
+            alert('回复评论');
             // 获取参数
             var $this = $(this);
             var news_id = $this.parent().attr('data-news-id');
@@ -234,6 +234,62 @@ $(function(){
             };
 
             // TODO 请求`回复评论`
+            $.ajax({
+                url: "/news/comment",
+                type: "post",
+                data: JSON.stringify(params),
+                contentType: "application/json",
+                headers: {
+                    "X-CSRFToken": getCookie("csrf_token")
+                },
+                success: function (resp) {
+                    if (resp.errno == "0") {
+                        // `回复评论`成功
+                        var comment = resp.comment;
+                        // 拼接内容
+                        var comment_html = "";
+                        comment_html += '<div class="comment_list">';
+                        comment_html += '<div class="person_pic fl">';
+                        if (comment.user.avatar_url) {
+                            comment_html += '<img src="' + comment.user.avatar_url + '" alt="用户图标">';
+                        }else {
+                            comment_html += '<img src="/static/news/images/person01.png" alt="用户图标">';
+                        }
+                        comment_html += '</div>';
+                        comment_html += '<div class="user_name fl">' + comment.user.nick_name + '</div>';
+                        comment_html += '<div class="comment_text fl">';
+                        comment_html += comment.content;
+                        comment_html += '</div>';
+                        comment_html += '<div class="reply_text_con fl">';
+                        comment_html += '<div class="user_name2">' + comment.parent.user.nick_name + '</div>';
+                        comment_html += '<div class="reply_text">';
+                        comment_html += comment.parent.content;
+                        comment_html += '</div>';
+                        comment_html += '</div>';
+                        comment_html += '<div class="comment_time fl">' + comment.create_time + '</div>';
+
+                        comment_html += '<a href="javascript:;" class="comment_up fr" data-comment-id="' + comment.id + '" data-news-id="' + comment.news_id + '">赞</a>';
+                        comment_html += '<a href="javascript:;" class="comment_reply fr">回复</a>';
+                        comment_html += '<form class="reply_form fl" data-comment-id="' + comment.id + '" data-news-id="' + news_id + '">';
+                        comment_html += '<textarea class="reply_input"></textarea>';
+                        comment_html += '<input type="button" value="回复" class="reply_sub fr">';
+                        comment_html += '<input type="reset" name="" value="取消" class="reply_cancel fr">';
+                        comment_html += '</form>';
+
+                        comment_html += '</div>';
+
+                        $(".comment_list_con").prepend(comment_html);
+                        // 请空输入框
+                        $this.prev().val('');
+                        // 关闭
+                        $this.parent().hide();
+                    }
+                    else {
+                        // `回复评论`失败
+                        alert(resp.errmsg);
+                    }
+                }
+            })
 
         }
     });
