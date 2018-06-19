50c5f4fa5f4c473823fe12025393b03a3c867017
smartlz <1009098051@qq.com>
2018-06-19 10:14:53 +0800

    用户中心-设置用户基本信息前端js代码实现

--- info/static/news/js/user_base_info.js
+++ info/static/news/js/user_base_info.js
@@ -6,10 +6,12 @@ function getCookie(name) {
 $(function () {
 
     $(".base_info").submit(function (e) {
+        // 阻止表单默认提交行为
         e.preventDefault();
 
         var signature = $("#signature").val();
         var nick_name = $("#nick_name").val();
+        // 获取用户选择的性别
         var gender = $(".gender:checked").val();
 
         if (!nick_name) {
@@ -28,6 +30,28 @@ $(function () {
         };
         
         // TODO 请求修改用户基本信息
+        $.ajax({
+            url: "/user/basic",
+            type: "post",
+            data: JSON.stringify(params),
+            contentType: "application/json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    // `设置`个人信息成功
+                    // 更新父窗口内容
+                    $('.user_center_name', parent.document).html(nick_name);
+                    $('#nick_name', parent.document).html(nick_name);
+                    $('.input_sub').blur()
+                }
+                else {
+                    // `设置`个人信息失败
+                    alert(resp.errmsg);
+                }
+            }
+        })
 
 
     })
