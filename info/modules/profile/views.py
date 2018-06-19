81ce82eb6c97678326616120a084c723be94f986
smartlz <1009098051@qq.com>
2018-06-19 09:55:05 +0800

    用户中心-基本信息页面显示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -5,6 +5,19 @@ from info.utils.commons import login_required
 from . import profile_blu
 
 
+# /user/basic
+@profile_blu.route('/basic')
+@login_required
+def user_basic():
+    """
+    用户中心-基本信息页面:
+    """
+    # 从g变量中获取user
+    user = g.user
+
+    return render_template('news/user_base_info.html', user=user)
+
+
 @profile_blu.route('')
 @login_required
 def get_user_profile():
