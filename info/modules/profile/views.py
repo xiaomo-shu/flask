1e1b8fa5eb87f45ff385bdc9fc26eaca51f9eff5
smartlz <1009098051@qq.com>
2018-06-19 10:04:31 +0800

    用户中心-设置用户基本信息后端views代码实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,12 +1,17 @@
-from flask import g
+from flask import current_app
+from flask import g, jsonify
 from flask import render_template
+from flask import request
+from flask import session
 
+from info import db
 from info.utils.commons import login_required
+from info.utils.response_code import RET
 from . import profile_blu
 
 
 # /user/basic
-@profile_blu.route('/basic')
+@profile_blu.route('/basic', methods=['GET', 'POST'])
 @login_required
 def user_basic():
     """
@@ -14,9 +19,45 @@ def user_basic():
     """
     # 从g变量中获取user
     user = g.user
+    
+    if request.method == 'GET':
+        return render_template('news/user_base_info.html', user=user)
+    else:
+        # 保存修改用户的信息
+        # 1. 接收参数并进行参数校验
+        req_dict = request.json
 
-    return render_template('news/user_base_info.html', user=user)
+        if not req_dict:
+            return jsonify(errno=RET.PARAMERR, errmsg='缺少参数')
 
+        signature = req_dict.get('signature')
+        nick_name = req_dict.get('nick_name')
+        gender = req_dict.get('gender')
+
+        if not all([signature, nick_name, gender]):
+            return jsonify(errno=RET.PARAMERR, errmsg='参数不完整')
+
+        if gender not in ('MAN', 'WOMAN'):
+            return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+
+        # 2. 设置用户的个人信息
+        user.signature = signature
+        user.nick_name = nick_name
+        user.gender = gender
+        
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            current_app.logger.error(e)
+            return jsonify(errno=RET.DBERR, errmsg='保存用户信息失败')
+
+        # 设置session中的nick_name
+        session['nick_name'] = nick_name
+        
+        # 3. 返回应答，设置个人信息成功
+        return jsonify(errno=RET.OK, errmsg='设置个人信息成功')
+            
 
 @profile_blu.route('')
 @login_required
