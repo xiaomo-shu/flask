39ea1508c6ca4fab959ea51309514188bc43a5ad
smartlz <1009098051@qq.com>
2018-06-20 12:13:11 +0800

    使用命令行命令添加`管理员`用户

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ from info import create_app, db, models
 
 # 需求: 只修改manage.py文件中的相应代码，
 # 获取不同配置环境下的app对象
+from info.models import User
+
 app = create_app('development')
 
 # 创建Manager对象
@@ -14,6 +16,29 @@ manager = Manager(app)
 Migrate(app, db)
 # 添加命令行数据库管理命令`db`
 manager.add_command('db', MigrateCommand)
+
+
+@manager.option('-n', '-name', dest='name')
+@manager.option('-p', '-password', dest='password')
+def createsuperuser(name, password):
+    """创建管理员用户"""
+    if not all([name, password]):
+        print('参数不足')
+        return
+
+    user = User()
+    user.mobile = name
+    user.nick_name = name
+    user.password = password
+    user.is_admin = True
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+        print("创建成功")
+    except Exception as e:
+        print(e)
+        db.session.rollback()
 
 
 if __name__ == '__main__':
