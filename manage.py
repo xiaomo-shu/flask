a59833142e362fad936c1c69dd66e216c25b3f50
smartlz <1009098051@qq.com>
2018-06-22 09:21:45 +0800

    测试用户的添加

--- manage.py
+++ manage.py
@@ -1,4 +1,7 @@
 # coding=utf-8
+import datetime
+import random
+
 from flask import current_app
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
@@ -41,7 +44,29 @@ def createsuperuser(name, password):
         db.session.rollback()
 
 
+def add_test_users():
+    users = []
+    now = datetime.datetime.now()
+    for num in range(0, 10000):
+        try:
+            user = User()
+            user.nick_name = "%011d" % num
+            user.mobile = "%011d" % num
+            user.password_hash = "pbkdf2:sha256:50000$SgZPAbEj$a253b9220b7a916e03bf27119d401c48ff4a1c81d7e00644e0aaf6f3a8c55829"
+            user.last_login = now - datetime.timedelta(seconds=random.randint(0, 2678400))
+            user.create_time = now - datetime.timedelta(seconds=random.randint(0, 2678400))
+            users.append(user)
+            print(user.mobile)
+        except Exception as e:
+            print(e)
+    with app.app_context():
+        db.session.add_all(users)
+        db.session.commit()
+    print('OK')
+
+
 if __name__ == '__main__':
     # 运行开发web服务器
     # app.run()
+    # add_test_users()
     manager.run()
